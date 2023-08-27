fa8ec9711d0eba84f57d587a97ecb09d8f94f4af
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2023-08-27 17:13:48 +0900

    [Silver I] Title: 점프, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.py
+++ 백준/Silver/1890. 점프/점프.py
@@ -1,21 +1,23 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
+# 2차원 배열 입력
 _list = [list(map(int, input().split())) for _ in range(N)]
+# 경로 개수 배열
 DP = [ [0 for _ in range(N)] for _ in range(N)]
+# 초기값 설정
 DP[0][0] = 1
 t = 0
 for i in range(N):
     for j in range(N):
+				# 점프 정의 : 오른쪽이나 아랫쪽으로 이동
         I, J = i + _list[i][j], j + _list[i][j]
-        if i == N-1 and j == N-1:
+        if i == N-1 and j == N-1: # 둘 다 N-1이 되는 경우 탈출.
             break
-        if I < N:
+        if I < N: # 오른쪽으로 이동하는 경우
             DP[I][j] += DP[i][j]
-            if _list[I][j] == 0:
-                continue
-        if J < N:
+
+        if J < N: # 아랫쪽으로 이동하는 경우
             DP[i][J] += DP[i][j]
-            if _list[i][J] == 0:
-                continue
+
 print(DP[N-1][N-1])
