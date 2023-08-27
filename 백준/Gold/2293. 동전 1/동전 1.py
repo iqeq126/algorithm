c01aec0aeb05a20468e941ca4bd60306be671567
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2023-08-27 17:18:05 +0900

    [Gold V] Title: 동전 1, Time: 224 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,15 +1,17 @@
 import sys
 input = sys.stdin.readline
-N, K = map(int, input().split())
-_list = []
-for i in range(N):
+N, K = map(int, input().split()) # N : 동전 개수, K : 가격합
+_list = [] # 저금통
+for i in range(N): # 동전 넣기
     t = int(input())
     _list.append(t)
-_list.sort()
-DP = [ 0 for i in range(K+1)]
-DP[0] = 1
-for i in range(1, N+1):
-    for j in range(_list[i-1], K+1):
-        DP[j] += DP[j - _list[i-1]]
+DP = [ 0 for i in range(K+1)] # 가격합의 경우의 수
+DP[0] = 1 # 초기값 설정
+# 1부터 하는 이유? 그렇게 하지 않으면 더하다가 오차가 생길 수 있음
+# ex) 1,2,5일 때 10을 만드는 경우. 0~9까지를 초기화한다면 
+# 5로 10을 만드는 경우같이 배수를 처리하기 까다로워짐
+for i in range(1, N+1): # 1 ~ N
+    for j in range(_list[i-1], K+1): # 가장 작은 동전 ~ K
+        DP[j] += DP[j - _list[i-1]] # 가진 동전을 빼나가면서 해당하는 동전을 더해줌
 
 print(DP[K])
