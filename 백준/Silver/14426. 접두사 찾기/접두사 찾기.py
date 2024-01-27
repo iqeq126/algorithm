95f21f3a6356989c2fc9adafd6a5ed57c4fcfc20
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-01-27 20:09:55 +0900

    [Silver I] Title: 접두사 찾기, Time: 348 ms, Memory: 121324 KB -BaekjoonHub

--- 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
@@ -1,22 +1,19 @@
 import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+sentence = sorted(input().strip() for _ in range(N))
+prefix = [input().strip() for _ in range(M)]
 
-N, M = map(int, sys.stdin.readline().split())
-sentence = sorted(sys.stdin.readline().strip() for _ in range(N))
-prefix = [sys.stdin.readline().strip() for _ in range(M)]
-
-def binary_search(sentence, prefix):
-    count = 0
-    for pre in prefix:
-        left, right = 0, len(sentence) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if sentence[mid].startswith(pre):
-                count += 1
-                break
-            elif sentence[mid] < pre:
-                left = mid + 1
-            else:
-                right = mid - 1
-    return count
-
-print(binary_search(sentence, prefix))
+res = 0
+for pre in prefix:
+    left, right = 0, len(sentence) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if sentence[mid].startswith(pre):
+            res += 1
+            break
+        elif sentence[mid] < pre:
+            left = mid + 1
+        else:
+            right = mid - 1
+print(res)
