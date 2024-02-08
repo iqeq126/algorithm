0319fd242e03a66a933cfd51574612743f2721b4
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-02-08 19:57:35 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,24 +1,17 @@
 import sys
 input = sys.stdin.readline
 def alphaCentauri(x, y):
-    res, k, dist = 0, 1, y-x
+    k, dist = 1, y-x
     if dist < 4:
-        res = dist
+        return dist
     else:
         while True:
             dist -= 2*k
             k+=1
-            if dist == 0:
-                res= 2*k - 2
-                break
-            elif 0 < dist <= k:
-                res= 2*k-1
-                break
-            elif k < dist < 2*k:
-                res= 2*k
-                break
-    print(res)
+            if dist == 0: return 2*k - 2
+            elif 0 < dist <= k: return 2*k-1
+            elif k < dist < 2*k: return 2*k
 t = int(input())
 for _ in range(t):
     x, y = map(int, input().split())
-    alphaCentauri(x,y)
+    print(alphaCentauri(x,y))
