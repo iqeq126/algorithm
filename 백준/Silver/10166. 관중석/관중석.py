696911bd33ff3de1fbe1a994886998ced2f0fcb8
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-02-22 16:52:30 +0900

    [Silver I] Title: 관중석, Time: 600 ms, Memory: 63904 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,8 +1,13 @@
-import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
-s = set()
-for i in range(N, M+1):
-    for j in range(1, i+1):
-        s.add(i/j)
-print(len(s))
+import math
+def getBleachers():
+    N, M = map(int, input().split())
+    s = [[False] * 2001 for _ in range(2001)]
+    res = 0
+    for i in range(N, M+1):
+        for j in range(1, i+1):
+            k = math.gcd(i, j)
+            if not s[i//k][j//k]:
+                res += 1
+                s[i//k][j//k] = True
+    print(res)
+getBleachers()
