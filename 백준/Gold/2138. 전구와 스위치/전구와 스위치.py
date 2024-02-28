d6694d65ac1848c7847531c21fc11797ebc35775
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-02-28 20:06:57 +0900

    [Gold V] Title: 전구와 스위치, Time: 156 ms, Memory: 33832 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -2,14 +2,14 @@ import sys, copy
 input = sys.stdin.readline
 # 0 : HIGH, 1 : LOW
 n = int(input())
-cur_state = list(map(int, input().rstrip()))
+cur_state = list(map(bool, map(int, input().rstrip())))
 copied_cur_state = copy.deepcopy(cur_state)
-res_state = list(map(int, input().rstrip()))
+res_state = list(map(bool, map(int, input().rstrip())))
 
 def bulb_switch(bulb, state, max_size=n):
     for i in range(state, state+3):
         if i >= max_size: continue
-        bulb[i] = ( bulb[i] + 1 ) % 2
+        bulb[i] = not bulb[i]
     return bulb
 
 def switch_times(cur_state, res_state, max_size=n, cnt = 0):
@@ -22,7 +22,7 @@ def switch_times(cur_state, res_state, max_size=n, cnt = 0):
 # 0번째 스위치를 누르는 경우
 result = sys.maxsize
 # 0, 1 switch 연산
-cur_state[0], cur_state[1] = (cur_state[0] + 1) % 2, (cur_state[1] + 1) % 2
+cur_state[0], cur_state[1] = not cur_state[0], not cur_state[1]
 res, cur_state = switch_times(cur_state, res_state)
 if cur_state == res_state: result = min(res+1, result)
 
