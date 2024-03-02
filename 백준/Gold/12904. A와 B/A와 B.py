ad56a3d8567c3e1f00c2f25c542bab4a58954ca2
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-02 18:58:29 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,8 +1,5 @@
 S = list(input())
 T = list(input())
-def AB(S, T):
-    while len(S) < len(T):
-        if T.pop() == 'B':
-            T = T[::-1]
-    print(1) if S==T else print(0)
-AB(S,T)
+while len(S) ^ len(T):
+    if T.pop() == 'B': T = T[::-1]
+print(1) if S==T else print(0)
