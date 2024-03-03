6d4a30a9d2272b53dad4b8c753ed993ea58aae41
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-03 18:33:14 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2260 ms, Memory: 73184 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,8 @@
+import sys, heapq
+input = sys.stdin.readline
+N = int(input())
+lst = []
+for _ in range(N):
+    heapq.heappush(lst, -int(input()))
+for _ in range(N):
+    print(-heapq.heappop(lst))
