cb9be2e5fbe5fc183b4ef17948f8349a7e2fc5b3
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-09 03:14:15 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 360 ms, Memory: 63424 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -4,17 +4,21 @@ input = sys.stdin.readline
 dic = defaultdict(list)
 N = int(input())
 dic[1] = [1]
-for _ in range(N-1):
-    x, y = map(int, input().split())
-    dic[x].append(y)
-    dic[y].append(x)
-q = deque([1])
-parent = defaultdict(int)
-while q:
-    cur = q.popleft()
-    for i in dic[cur]:
-        if parent[i] == 0 and i != 1:
-            parent[i] = cur
-            q.append(i)
-for _ in range(2, N+1):
-    print(parent[_])
+def setDict():
+    for _ in range(N-1):
+        x, y = map(int, input().split())
+        dic[x].append(y)
+        dic[y].append(x)
+def getParent():
+    q = deque([1])
+    parent = defaultdict(int)
+    while q:
+        cur = q.popleft()
+        for i in dic[cur]:
+            if parent[i] == 0 and i != 1:
+                parent[i] = cur
+                q.append(i)
+    for _ in range(2, N+1):
+        print(parent[_])
+setDict()
+getParent()
