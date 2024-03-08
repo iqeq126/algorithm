bd54806248fe3a381c42d615c2e036a64ea494f2
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-09 02:05:55 +0900

    [Platinum I] Title: n제곱 계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/12728. n제곱 계산/n제곱 계산.py
+++ 백준/Platinum/12728. n제곱 계산/n제곱 계산.py
@@ -1,5 +1,5 @@
 import sys
-print = sys.stdout.write
+input, print = sys.stdin.readline, sys.stdout.write
 dxyz = [list(map(int, format(i, '03b'))) for i in range(8)]
 def multi(a, b):
     matrix = [[0,0], [0,0]]
@@ -13,7 +13,6 @@ def square(x, n):
         if n % 2: return multi(multi(matrix, matrix), x)
         else: return multi(matrix, matrix)
 for _ in range(int(input())):
-    N = int(input())
     matrix = [[6, -4], [1, 0]]
-    res = square(matrix, N)
+    res = square(matrix, int(input()))
     print(f"Case #{_+1}: {(res[0][0] + res[1][1] -1)%1000:0>3}\n")
