04b97ac3d393ae49f7ad9340d49f040935dbb3ed
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-09 03:44:36 +0900

    [Silver I] Title: 경로 찾기, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,10 +1,10 @@
 N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
 def floyd_warshall():
+    graph = [list(map(int, input().split())) for _ in range(N)]
     for i in range(N):
         for j in range(N):
             for k in range(N):
                 if graph[j][i] != 0 and graph[i][k] != 0:
                     graph[j][k] = 1
-floyd_warshall()
-[ print(*graph[i]) for i in range(N)]
+    return graph
+print('\n'.join(' '.join(map(str, row)) for row in floyd_warshall()))
