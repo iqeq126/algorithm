52c7240362ae4aa16c46e4002c83499c6a461d02
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-03-12 05:09:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,10 @@
 import sys
+input, print = sys.stdin.readline, sys.stdout.write
 def is_valid():
     case = int(input())
     phone_book = []
     for _ in range(case):
-        phone_book.append(sys.stdin.readline().rstrip("\n"))
+        phone_book.append(input().rstrip("\n"))
     phone_book.sort()
     for i in range(case-1):
         area_code = len(phone_book[i])
@@ -11,6 +12,5 @@ def is_valid():
             return "NO"
     return "YES"
 
-
-for _ in range(int(sys.stdin.readline())):
-    print(is_valid())
+for _ in range(int(input())):
+    print(f"{is_valid()}\n")
