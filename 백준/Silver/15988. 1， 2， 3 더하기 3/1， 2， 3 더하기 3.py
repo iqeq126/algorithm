03095b5ae96ee6e99b03ae3dbc5f319a3e421b6f
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-05-09 02:54:40 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 452 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,10 @@
+import sys
+input, print = sys.stdin.readline, sys.stdout.write
+dp = [0, 1, 2, 4] + [0] * 1_000_005
+MOD = 1_000_000_009
+for i in range(4, 1000004):
+    dp[i] = ( dp[i-1] + dp[i-2] + dp[i-3] ) % MOD
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(f"{dp[n] % MOD}\n")
