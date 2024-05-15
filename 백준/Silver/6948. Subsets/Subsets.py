67dc2a6cd6124122e883fb08cb584f22149b0de5
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-05-15 14:36:38 +0900

    [Silver III] Title: Subsets, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6948. Subsets/Subsets.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+s = [set() for _ in range(26)]
+visited = [False for _ in range(26)]
+cmd = sorted([list(input().split()) for i in range(n)], key=lambda x: x[2], reverse=True)
+
+for i in range(n):
+    megasets, _, subsets = cmd[i]
+    mega, sub = ord(megasets)-ord('A'), ord(subsets)-ord('A')
+    visited[mega] = True
+    if ord(subsets) <= ord('Z'):
+        s[mega] = set.union(s[mega], s[sub])
+        visited[sub] = True
+    else:
+        s[mega].add(subsets)
+
+cmd.sort()
+for i in range(n):
+    megasets, _, subsets = cmd[i]
+    mega, sub = ord(megasets)-ord('A'), ord(subsets)-ord('A')
+    visited[mega] = True
+    if ord(subsets) <= ord('Z'):
+        s[mega] = set.union(s[mega], s[sub])
+        visited[sub] = True
+    else:
+        s[mega].add(subsets)
+
+for i in range(26):
+    if visited[i]:
+        print(f"{chr(ord('A')+i)} = ", end="")
+        print('{', end="")
+        print(*sorted(s[i]), sep=",", end="")
+        print('}')
