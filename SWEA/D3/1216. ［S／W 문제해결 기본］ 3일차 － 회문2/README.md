4829491d732fc56a32c7b8df4113f90af113275f
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-05-17 21:33:13 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,477 ms, Memory: 60,740 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/README.md
@@ -0,0 +1,15 @@
+# [D3] [S/W 문제해결 기본] 3일차 - 회문2 - 1216 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14Rq5aABUCFAYi) 
+
+### 성능 요약
+
+메모리: 60,740 KB, 시간: 2,477 ms, 코드길이: 942 Bytes
+
+### 제출 일자
+
+2024-05-17 21:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,40 @@
+# 배열 범위 검증
+def is_valid(a):
+    return 0 <= a < 100
+
+# 팰린드롬 검사
+def is_palindrome(word, res):
+    palindrome = word[::-1]
+    if word == palindrome:
+        return max(res, len(word))
+    else:
+        return res
+
+
+for _ in range(1, 11):
+    n = int(input())
+    res = 0
+    board = [list(input()) for _ in range(100)]
+
+
+    for i in range(100):
+        for j in range(100):
+            word = []
+            for k in range(100):
+                if is_valid(j + k):
+                    word.append(board[i][j + k])
+                else:
+                    continue
+                res = is_palindrome(word, res)
+
+    for j in range(100):
+        for i in range(100):
+            word = []
+            for k in range(100):
+                if is_valid(i + k):
+                    word.append(board[i + k][j])
+                else:
+                    continue
+                res = is_palindrome(word, res)
+
+    print(f"#{n} {res}")
