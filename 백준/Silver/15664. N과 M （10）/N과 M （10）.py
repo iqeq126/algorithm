58249927b1f9ccf8e1f4429d4132742aa228087c
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-05-19 20:19:27 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,19 @@
+import sys
+sys.setrecursionlimit(10**8)
+print = sys.stdout.write
+n, m = map(int, input().split())
+lst = sorted(list(map(int, input().split())))
+def NandM(s, res):
+    if len(res) == m:
+        for i in range(m):
+            print(f"{res[i]} ")
+        print("\n")
+        return
+    buf = 0
+    for i in range(s, n):
+        if buf < lst[i]:
+            res.append(lst[i])
+            buf = lst[i]
+            NandM(i+1, res)
+            res.pop()
+NandM(0, [])
