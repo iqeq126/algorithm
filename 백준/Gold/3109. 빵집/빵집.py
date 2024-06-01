45981156f6abcd410359c78a242d7464c55bc625
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-06-01 15:46:17 +0900

    [Gold II] Title: 빵집, Time: 1324 ms, Memory: 359612 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,9 +1,8 @@
 import sys
 sys.setrecursionlimit(10**4)
-input = sys.stdin.readline
-r, c = map(int, input().split())
+r, c = map(int, sys.stdin.readline().split())
 res = 0
-board = list(list(input().rstrip('\n')) for _ in range(r))
+board = list(list(sys.stdin.readline().rstrip('\n')) for _ in range(r))
 
 
 def is_valid(y, x):
@@ -12,7 +11,8 @@ def is_valid(y, x):
 
 def dfs(y, x):
     if x == c-1:
-        return 1
+        return True
+
     nx = x+1
     for dy in (-1,0,1):
         ny = y+dy
