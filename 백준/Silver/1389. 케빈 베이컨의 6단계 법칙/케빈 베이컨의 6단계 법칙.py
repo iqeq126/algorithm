e8acba4d2be4a781092e066d4deb46b35756691f
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-06-04 06:21:39 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 176 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,32 +1,35 @@
 import sys
-from collections import defaultdict, deque
-
 input = sys.stdin.readline
 n, m = map(int, input().split())
-users = defaultdict(list)
+users = [ [sys.maxsize] * (n+1) for _ in range(n+1)]
+
+
+def pass_through(i, k, j):
+    return users[i][k] + users[k][j]
+
+
+def floyd():
+    for k in range(1, n+1):
+        users[k][k] = 0
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if i == j: continue
+                if users[i][j] > pass_through(i, k, j):
+                    users[i][j] = pass_through(i, k, j)
+
 for i in range(m):
     a, b = map(int, input().split())
-    users[a].append(b)
-    users[b].append(a)
+    users[a][b] = 1
+    users[b][a] = 1
 
-def bfs(s):
-    q = deque()
-    q.append(s)
-    while q:
-        cur = q.popleft()
-        for nxt in users[cur]:
-            if visited[nxt] == 0 and s != nxt:
-                visited[nxt] = visited[cur] + 1
-                q.append(nxt)
+floyd()
 
 res = sys.maxsize
-min_user_idx = 0
+min_users_num = sys.maxsize
 for i in range(1, n+1):
-    visited = [0] * (n+1)
-    bfs(i)
-    local_res = sum(visited)
+    local_res = sum(users[i][1:])
     if res > local_res:
         res = local_res
-        min_user_idx = i
+        min_users_num = i
 
-print(min_user_idx)
+print(min_users_num)
