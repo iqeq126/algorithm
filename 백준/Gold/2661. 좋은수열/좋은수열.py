137b98c14ace192cd599adaa407089ffeba8c52a
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-07-28 12:54:07 +0900

    [Gold IV] Title: 좋은수열, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,19 +1,14 @@
-import sys
-sys.setrecursionlimit(10**5)
 n = int(input())
-res = ""
 def isGood(length, cur):
     for i in range(1, length // 2 + 1):
-        for j in range(0, length - 2 * i + 1):
+        for j in range(length - 2 * i + 1):
             if cur[j:j + i] == cur[j + i:j + 2 * i]:
                 return False
     return True
-
 def getList(num, cur):
     if n == num:
-        res = cur
         print(cur)
-        sys.exit(0)
+        exit(0)
     lst = "123"
     for i in lst:
         if isGood(num+1, cur+i):
