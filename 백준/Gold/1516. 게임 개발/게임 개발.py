cc8be8ebc250c4a0c695c1c425acdd50069166f3
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2024-08-11 21:15:24 +0900

    [Gold III] Title: 게임 개발, Time: 104 ms, Memory: 36184 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -0,0 +1,36 @@
+import sys, heapq
+from collections import deque, defaultdict
+input, print = sys.stdin.readline, sys.stdout.write
+N = int(input())
+g = defaultdict(list)
+dependency = [0] * (N+1)
+build_time = [0] * (N+1)
+total_build_time = [0] * (N+1)
+for i in range(1, N+1):
+    argv = deque(map(int, input().split()))
+    build_time[i] = argv.popleft()
+    while argv:
+        argc = argv.popleft()
+        if argc == -1:
+            break
+        g[argc].append(i)
+        dependency[i] += 1
+
+q = deque()
+for i in range(1, N+1):
+    if dependency[i] == 0:
+        q.append(i)
+        total_build_time[i] = build_time[i]
+
+while q:
+    now = q.popleft()
+    for nxt in g[now]:
+        total_build_time[nxt] = max(total_build_time[nxt],
+                                    total_build_time[now] + build_time[nxt])
+        dependency[nxt] -= 1
+        if dependency[nxt] == 0:
+            q.append(nxt)
+
+total_build_time = total_build_time[1:]
+for t in total_build_time:
+    print(f"{t}\n")
